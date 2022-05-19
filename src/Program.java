91d65a8a03e89d631fb00edb92b31d5c5b3bc8dd
Dmitiry Matveev <dma.github@gmail.com>
2022-05-19 20:52:04 +0300

    Created a static method

--- src/Program.java
+++ src/Program.java
@@ -2,23 +2,10 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-		kislushchanka();
-		turomsha();
-		busel();
+		matveev();
 	}
 
-	public static void kislushchanka(){
-		System.out.println("Кислущенко Андрей Валерьевич");
-	}
-
-	/**
-	 * This method shows my full name
-	 */
-	public static void turomsha(){
-		System.out.println("Туромша Тимофей Павлович");
-
-	}
-	public static void busel(){
-		System.out.println("Денис Бусел!");
+	public static void matveev (){
+		System.out.println("Dmitriy Matveev");
 	}
 }
