b6c730ec1a2d0bb264a6fac272a1e7b5f502c156
andrei-kislushchanka <Andrei.Kislushchanka@sanofi.com>
2022-05-19 17:51:00 +0300

    added my method

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,10 @@ public class Program
 {
 	public static void main(String[] args)
 	{
+		kislushchanka();
+	}
 
+	public static void kislushchanka(){
+		System.out.println("Кислущенко Андрей Валерьевич");
 	}
 }
