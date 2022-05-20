5569e39232078b84ce8c8652a94179a1afa60002
Tsimafey <somebody7527@gmail.com>
2022-05-20 15:50:12 +0300

    This is a new method, that shows my full name

--- src/Program.java
+++ src/Program.java
@@ -8,4 +8,11 @@ public class Program
 	public static void kislushchanka(){
 		System.out.println("Кислущенко Андрей Валерьевич");
 	}
+
+	/**
+	 * This method shows my full name
+	 */
+	public static void turomsha(){
+		System.out.println("Туромша Тимофей Павлович");
+	}
 }
