982b7e46f08fd5321bebe5241e779a3f31ffc371
lizar <maki3.14@mail.ru>
2022-05-22 20:49:09 +0300

    Arxipov

--- src/Program.java
+++ src/Program.java
@@ -1,24 +1,9 @@
-public class Program
-{
-	public static void main(String[] args)
-	{
-		kislushchanka();
-		turomsha();
-		busel();
-	}
+public class Program {
+    public static void main(String[] args) {
+        arxipov();
+    }
 
-	public static void kislushchanka(){
-		System.out.println("Кислущенко Андрей Валерьевич");
-	}
-
-	/**
-	 * This method shows my full name
-	 */
-	public static void turomsha(){
-		System.out.println("Туромша Тимофей Павлович");
-
-	}
-	public static void busel(){
-		System.out.println("Денис Бусел!");
-	}
+    public static void arxipov() {
+        System.out.println("Архипов Георгий Владимирович");
+    }
 }
