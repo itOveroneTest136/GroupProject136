e55b589b4e7a420b5322a575e74f346a27df6e43
Dmitiry Matveev <dma.github@gmail.com>
2022-05-22 17:18:35 +0300

    Created a static method

--- src/Program.java
+++ src/Program.java
@@ -2,10 +2,11 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
+
 		kislushchanka();
 		turomsha();
 		busel();
+		matveev();
 	}
 
 	public static void kislushchanka(){
@@ -21,12 +22,12 @@ public class Program
 	}
 	public static void busel(){
 		System.out.println("Денис Бусел!");
-=======
-		matveev();
+
+
 	}
 
 	public static void matveev (){
 		System.out.println("Dmitriy Matveev");
->>>>>>> 241af7d (Created a static method)
+
 	}
 }
