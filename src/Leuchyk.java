c25cfb61ac46836e4f9942d85a320b6dddc7ba27
Nemiroff794 <vainem87@gmail.com>
2022-05-26 18:29:52 +0300

    Leuchyk enum2

--- src/Leuchyk.java
+++ src/Leuchyk.java
@@ -2,9 +2,7 @@ public enum Leuchyk
 {
 	BELARUS(9_413_446, 9_349_645),
 	BRITISH(67_337_516, 68_134_268),
-	SPANISH(46_937_060, 47_344_649)
-	;
-
+	SPANISH(46_937_060, 47_344_649);
 
 	private int population2019;
 	private int population2021;
