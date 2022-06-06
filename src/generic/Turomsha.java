7014af5315041398e8cb1799777fb204c4bf31ce
Tsimafey <somebody7527@gmail.com>
2022-06-06 20:36:47 +0300

    This feature now includes generic!

---
+++ src/generic/Turomsha.java
@@ -0,0 +1,20 @@
+package generic;
+
+public class Turomsha <T extends Number,E >{
+    public T value;
+    private E value2;
+
+    public Turomsha(T value, E value2) {
+        this.value = value;
+        this.value2 = value2;
+    }
+
+    public E getValue2() {
+        return value2;
+    }
+
+    public void setValue2(E value2) {
+        this.value2 = value2;
+    }
+
+}
