bc527160aae42cb9596b8966ed8738e7d28fbfa3
Nemiroff794 <vainem87@gmail.com>
2022-06-06 19:44:08 +0300

    Leuchyk Exceptions 1

--- src/Program.java
+++ src/Program.java
@@ -31,76 +31,100 @@ public class Program
 		samok();
 	}
 
-	public static void kislushchanka(){
+	public static void kislushchanka()
+	{
 		System.out.println("Кислущенко Андрей Валерьевич");
 	}
 
 	/**
 	 * This method shows my full name
+	 *
 	 * @author Туромша Тимофей
 	 */
-	public static void turomsha(){
+	public static void turomsha()
+	{
 		System.out.println("\nТуромша Тимофей Павлович");
 		System.out.println("Уругвай, 2019 - " + Turomsha.URUGUY.getPopulation2019() + " чел.");
-		System.out.println("Польша, 2020 - " + Turomsha.POLAND.getPopulation2020()+ " чел.");
-		try {
+		System.out.println("Польша, 2020 - " + Turomsha.POLAND.getPopulation2020() + " чел.");
+		try
+		{
 			throw new TuromshaException("АШИБКО");
-		} catch (TuromshaException e) {
-			System.out.println(e.getDetails()+ "\n");
+		}
+		catch (TuromshaException e)
+		{
+			System.out.println(e.getDetails() + "\n");
 		}
 	}
 
-	public static void busel(){
+	public static void busel()
+	{
 		System.out.println("Денис Бусел!");
 		System.out.println("Население Беларуси в 2019 = " + Busel.BELARUS.getPopulation2019());
 		System.out.println("Население Росии в 2021 = " + Busel.RUSSIA.getPopulation2021());
-		try {
+		try
+		{
 			throw new BuselException("Ситуация...");
-		} catch (BuselException e) {
+		}
+		catch (BuselException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void novik(){
+	public static void novik()
+	{
 		System.out.println("Новик Сергей");
 		System.out.println("Население Беларуси в 2019 = " + Busel.BELARUS.getPopulation2019());
 		System.out.println("Население Росии в 2021 = " + Busel.RUSSIA.getPopulation2021());
 	}
 
-	public static void matveev (){
+	public static void matveev()
+	{
 		System.out.println("Dmitriy Matveev");
 		System.out.println("Население Беларуси в 2019 году = " + Matveev.BELARUS.getPopulation2019());
 		System.out.println("Население Беларуси в 2021 году = " + Matveev.BELARUS.getPopulation2021());
-		try {
+		try
+		{
 			throw new MatveevException("МЯСОУБОРОЧНЫЙ КОМБАЙН");
-		} catch (MatveevException e) {
+		}
+		catch (MatveevException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void beliayeva() {
+	public static void beliayeva()
+	{
 		System.out.println("Беляева Наталья Александровна");
 		System.out.println("Беларусь 2019 = " + Beliayeva.BELARUS.getPopulation2019());
 		System.out.println("Беларусь 2021 = " + Beliayeva.BELARUS.getPopulation2021());
-		try {
+		try
+		{
 			throw new BeliayevaException("поломка программы");
-		} catch (BeliayevaException e) {
+		}
+		catch (BeliayevaException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void plyosov() {
+	public static void plyosov()
+	{
 		System.out.println("Плёсов Дмитрий Александрович");
 		System.out.println("Беларусь (2019) население = " + plyosov.BELARUS.getPopulation2019());
 		System.out.println("Беларусь (2021) население = " + plyosov.BELARUS.getPopulation2021());
-		try {
+		try
+		{
 			throw new PlyosovException(" ПалЯмалсО,  Апасна Апасна !!!");
-		} catch (PlyosovException e) {
+		}
+		catch (PlyosovException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void leuchyk(){
+	public static void leuchyk()
+	{
 		System.out.println();
 		System.out.println("Левчик В С");
 		System.out.println("Население Испании в 2019 году: " + Leuchyk.SPANISH.getPopulation2019());
@@ -116,36 +140,49 @@ public class Program
 
 	}
 
-	public static void Konstantinovich() {
+	public static void Konstantinovich()
+	{
 		System.out.println("Konstantinovich  Svetlana Lvovna");
-		System.out.println("Belarus (2019)="+Konstantinovi.BELARUS.getPopulatoin2019());
-		System.out.println("Belarus (2021)="+Konstantinovi.BELARUS.getPopulatoin2021());
-		try {
+		System.out.println("Belarus (2019)=" + Konstantinovi.BELARUS.getPopulatoin2019());
+		System.out.println("Belarus (2021)=" + Konstantinovi.BELARUS.getPopulatoin2021());
+		try
+		{
 			throw new KonstantsinovichException(" сломал");
-		} catch (KonstantsinovichException e) {
+		}
+		catch (KonstantsinovichException e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void budnik() {
+	public static void budnik()
+	{
 		System.out.println("Будник Артём Валерьевич");
 		System.out.println("Беларусь (2019) Население" + budnik.BELARUS.getPopulation2019());
 		System.out.println("Беларусь (2021)Население" + budnik.BELARUS.getPopulation2021());
-		try {
+		try
+		{
 			throw new BudnikExceptions("попортил");
-		} catch (BudnikExceptions e) {
+		}
+		catch (BudnikExceptions e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void samok(){
+	public static void samok()
+	{
 		System.out.println("Samok Iryna");
 		System.out.println("Украина (2019) = " + Samok.UKRAINE.getPopulation2019());
 		System.out.println("Украина (2021) = " + Samok.UKRAINE.getPopulation2021());
-		try {
+		try
+		{
 			throw new SamokException("сломала");
-		} catch (SamokException e) {
+		}
+		catch (SamokException e)
+		{
 			e.printStackTrace();
 		}
 	}
+
 }
